9b906b23c6b8782c296ec1b7c505d88f6e0ed832
Muhammad Amin Hidayat <tumbaldaya@gmail.com>
2025-12-25 12:12:51 +0800

    fix: image responsive mbile

--- src/app/_components/home-hero/home-hero-decoration.styles.ts
+++ src/app/_components/home-hero/home-hero-decoration.styles.ts
@@ -25,10 +25,9 @@ export const animatedDecorations: Array<AnimateDecoration> = [
     sxRoot: {
       top: { xs: -20, md: -30 },
       right: { xs: 10, md: 40 },
-      display: { xs: 'none', sm: 'block' },
     },
     sxImgContainer: {
-      width: { xs: 150, sm: 200, md: 330 },
+      width: { xs: 100, sm: 200, md: 330 },
     },
     initial: { rotate: -20, opacity: 0, scale: 2, x: 200 },
     animate: { rotate: 0, opacity: 1, scale: 1, x: 0 },
@@ -47,10 +46,9 @@ export const animatedDecorations: Array<AnimateDecoration> = [
     sxRoot: {
       top: { xs: -10, md: 0 },
       left: { xs: -20, md: 0 },
-      display: { xs: 'none', sm: 'block' },
     },
     sxImgContainer: {
-      width: { xs: 120, sm: 180, md: 250 },
+      width: { xs: 80, sm: 180, md: 250 },
     },
     initial: { rotate: 10, opacity: 0, scale: 1.25, x: -100 },
     animate: { rotate: 0, opacity: 1, scale: 1, x: 0 },
@@ -70,10 +68,9 @@ export const animatedDecorations: Array<AnimateDecoration> = [
       left: { xs: 134, md: 234 },
       bottom: { xs: 172, md: 272 },
       transform: 'rotate(-45deg)',
-      display: { xs: 'none', lg: 'block' },
     },
     sxImgContainer: {
-      width: { xs: 25, md: 40 },
+      width: { xs: 20, md: 40 },
     },
     initial: { rotate: -50, opacity: 0, y: 400 },
     animate: { rotate: 0, opacity: 1, y: 0 },
@@ -93,10 +90,9 @@ export const animatedDecorations: Array<AnimateDecoration> = [
       left: { xs: 174, md: 274 },
       bottom: { xs: 136, md: 216 },
       transform: 'rotate(13deg)',
-      display: { xs: 'none', lg: 'block' },
     },
     sxImgContainer: {
-      width: { xs: 25, md: 40 },
+      width: { xs: 20, md: 40 },
     },
     initial: { rotate: -50, opacity: 0, y: 400 },
     animate: { rotate: 0, opacity: 1, y: 0 },
@@ -115,10 +111,9 @@ export const animatedDecorations: Array<AnimateDecoration> = [
     sxRoot: {
       left: { xs: -60, md: -110 },
       bottom: { xs: -100, md: -160 },
-      display: { xs: 'none', md: 'block' },
     },
     sxImgContainer: {
-      width: { xs: 280, sm: 380, md: 520 },
+      width: { xs: 180, sm: 380, md: 520 },
     },
     initial: { rotate: 0, opacity: 0, y: 300 },
     animate: { rotate: 0, opacity: 1, y: 0 },
@@ -137,10 +132,9 @@ export const animatedDecorations: Array<AnimateDecoration> = [
     sxRoot: {
       left: { xs: 170, md: 270 },
       bottom: { xs: 66, md: 106 },
-      display: { xs: 'none', sm: 'block' },
     },
     sxImgContainer: {
-      width: { xs: 80, sm: 100, md: 148 },
+      width: { xs: 60, sm: 100, md: 148 },
     },
     initial: { rotate: -50, opacity: 0, y: 400 },
     animate: { rotate: 0, opacity: 1, y: 0 },
@@ -159,10 +153,9 @@ export const animatedDecorations: Array<AnimateDecoration> = [
     sxRoot: {
       bottom: { xs: -20, md: 0 },
       left: { xs: 60, md: 100 },
-      display: { xs: 'none', md: 'block' },
     },
     sxImgContainer: {
-      width: { xs: 100, md: 200 },
+      width: { xs: 70, md: 200 },
     },
     initial: { rotate: -50, opacity: 0, y: 400 },
     animate: { rotate: 0, opacity: 1, y: 0 },
@@ -181,10 +174,9 @@ export const animatedDecorations: Array<AnimateDecoration> = [
     sxRoot: {
       right: { xs: -40, md: -60 },
       bottom: { xs: -50, md: -84 },
-      display: { xs: 'none', sm: 'block' },
     },
     sxImgContainer: {
-      width: { xs: 220, sm: 320, md: 460 },
+      width: { xs: 150, sm: 320, md: 460 },
     },
     initial: { rotate: 0, opacity: 0, scale: 1.1, x: 200 },
     animate: { rotate: -32, opacity: 1, scale: 1, x: 0 },
@@ -203,10 +195,9 @@ export const animatedDecorations: Array<AnimateDecoration> = [
     sxRoot: {
       bottom: { xs: -60, md: -90 },
       right: { xs: 180, sm: 240, md: 320 },
-      display: { xs: 'none', sm: 'block' },
     },
     sxImgContainer: {
-      width: { xs: 120, sm: 150, md: 206 },
+      width: { xs: 90, sm: 150, md: 206 },
     },
     initial: { rotate: 20, opacity: 0, scale: 1.1, x: 200 },
     animate: { rotate: -6, opacity: 1, scale: 1, x: 0 },
